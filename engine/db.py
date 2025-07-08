2893467f584263faad14cdc0a099b7d82021c346
Amitosh <amitosh1206@gmail.com>
2025-07-08 23:14:13 +0530

    update

--- engine/db.py
+++ engine/db.py
@@ -5,9 +5,4 @@ conn = sqlite3.connect('amishra.db')
 cursor = conn.cursor()
 
 query = "CREATE TABLE IF NOT EXISTS sys_command(id integer primary key, name VARCHAR(100), path VARCHAR(1000))"
-
-query = "DROP TABLE Amitosh"
-if(cursor.execute(query)):
-    print("Query Exucuted successfully")
-else:
-    print("Failed to Run Query")   
+cursor.execute(query)

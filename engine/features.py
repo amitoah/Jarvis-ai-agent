345b40f9be13d10cda0fa74ea3a13a454d952aeb
Amitosh <amitosh1206@gmail.com>
2025-07-09 17:53:05 +0530

    import cursor

--- engine/features.py
+++ engine/features.py
@@ -1,5 +1,9 @@
 import os
 import re
+import webbrowser
+import sqlite3
+from engine.db import cursor, con as conn
+# Importing playsound for playing audio files
 from playsound import playsound 
 import eel
 from engine.command import speak
@@ -15,15 +19,49 @@ def playAssistantSound():
 def openCommand(query):
     query = query.replace(ASSISTANT_NAME, "")
     query = query.replace("open", "")
-    query = query.lower().strip()
-    if query == "youtube":
-        speak("Opening YouTube")
-        os.system("start https://www.youtube.com")
-    elif query != "":
-        speak("Opening " + query)
-        os.system("start " + query)
-    else:
-        speak("not found")
+    query = query.lower()
+
+    
+    # if query == "youtube":
+    #     speak("Opening YouTube")
+    #     os.system("start https://www.youtube.com")
+    # elif query != "":
+    #     speak("Opening " + query)
+    #     os.system("start " + query)
+    # else:
+    #     speak("not found")
+    app_name = query.strip()
+
+    if app_name != "":
+
+        try:
+            cursor.execute(
+                'SELECT path FROM sys_commznd WHERE nmae IN (?)', (app_name))
+            results = cursor.fetchall()
+            
+            if len(results) !=0:
+                speak("Opening "+query)
+                os.startfile(results[0][0])
+
+            elif len(results) == 0:
+                cursor.execute(
+                'SELECT url FROM web_command WHERE name IN (?)', (app_name, ))
+                results = cursor.fetchall()
+
+                if len(results) != 0:
+                    speak("Opening "+query)
+                    webbrowser.open(results[0][0])
+
+                else:
+                    speak("Opening "+query)
+                    try:
+                        os.system('strat '+query)
+                    except:
+                        speak("not found")
+        except:
+            speak("some thing went wrong")
+    
+
 
 def playYoutube(query):
     serch_term = extract_yt_term(query)
